4a376c55815813e7bbb168c2f31c0ffc69ecf9b9
Per Almstrom <per.almstrom@bambuser.com>
2023-05-31 22:26:02 +0200

    Allow cross-origin requests from swagger-ui

--- src/main/java/se/sodalabs/hub/api/RegistrationController.java
+++ src/main/java/se/sodalabs/hub/api/RegistrationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import se.sodalabs.hub.domain.Participant;
 import se.sodalabs.hub.repository.ConnectedParticipantsRepository;
 import se.sodalabs.hub.views.dashboard.ParticipantListDataProvider;
 
+@CrossOrigin(origins = "http://localhost:9090")
 @RestController
 public class RegistrationController {
 
