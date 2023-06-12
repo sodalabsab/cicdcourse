f47fbc8ae197561e1c8dc733d2863dc40bf24698
Per Almstrom <per.almstrom@gmail.com>
2023-06-12 21:23:15 +0200

    Hopefully solve generated openapi url pointing to correct scheme (https)

--- src/main/java/se/sodalabs/hub/HubApplication.java
+++ src/main/java/se/sodalabs/hub/HubApplication.java
@@ -4,6 +4,8 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Theme(value = "hub", variant = Lumo.DARK)
 @Push
+@OpenAPIDefinition(
+		servers = { @Server(url = "/", description = "Default Server URL") }
+)
 public class HubApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
